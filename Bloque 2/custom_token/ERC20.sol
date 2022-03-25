892ee09d8e266d1a3dcd29a3aa28784518482639
davidrs7 <davidrodriguezrivera0@gmail.com>
2022-03-25 18:15:18 -0500

    Finish ERC20 token

--- Bloque 2/custom_token/ERC20.sol
+++ Bloque 2/custom_token/ERC20.sol
@@ -2,6 +2,13 @@
 pragma solidity ^0.8.0;
 import  "./SafeMath.sol";
 
+
+/*
+Luciana Rodriguez - 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
+Carolina Cantor - 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+David Rodriguez - 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+*/
+
 // interface de token ERC20
 interface IERC20{
     //devuelve cantidad de tokens en existencia
@@ -61,18 +68,35 @@ contract ERC20Basic is IERC20{
     }
 
     //devuelve un valor bool resultado de la operación indicada(transferencia)
-    function transfer(address recipient,uint256 amount)public override  returns(bool){
-        return false;
+    function transfer(address recipient,uint256 numTokens)public override  returns(bool){
+        require(numTokens <= balances[msg.sender] , "Supera la cantidad disponible");
+        balances[msg.sender] = balances[msg.sender].sub(numTokens);
+        balances[recipient] = balances[recipient].add(numTokens);
+        emit Transfer(msg.sender,recipient,numTokens);
+        return true;
     }
  
     //devuelve un valor bool con el resultado con el valor de gasto
-    function approve(address spender,uint256 ammout) public override returns(bool){
-        return false;
+    function approve(address delegate,uint256 numTokens) public override returns(bool){
+        allowed[msg.sender][delegate] = numTokens;
+        emit Approval(msg.sender,delegate,numTokens);
+        return true;
     }
 
         //devuelve un valor booleano con el resultado de la operación de paso de una cantidad de tokens usando allowance()
-    function transferFrom(address sender,address recipient,uint256 ammout) public override returns(bool){
-        return false;
+    function transferFrom(address owner,address buyer,uint256 numTokens) public override returns(bool){
+        //el propietario debe disponer de los tokens a comprar
+        require(numTokens <= balances[owner], "El propietario no cuenta con la disponibilidad de los tokens a comprar");        
+        //el numro de Tokens debe ser menor o igual a los tokens aprovados 
+        require(numTokens <= allowed[owner][msg.sender], "No es posible aprobar la transaccion.");
+        //Se resta el numero de tokens al propietario.
+        balances[owner] = balances[owner].sub(numTokens);
+        //Se resta el numero de tokens al intermediario.
+        allowed[owner][msg.sender] = allowed[owner][msg.sender].sub(numTokens);
+        //Se adiciona el numero de tokens al comprador.
+        balances[buyer] = balances[buyer].add(numTokens); 
+        emit Transfer(owner,buyer,numTokens);
+        return true;
     }
     
 }
